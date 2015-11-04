b2133ee9a68de60e6e433050ce22dfbb8c7ae957
Jonathan Sterling <jon@jonmsterling.com>
2015-11-04 15:53:21 -0800

    Add test/example

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,33 @@
 module Test.Main where
 
 import Prelude
+
+import Data.Maybe
+import Data.Either
+import Data.Functor (($>))
+
+import Control.Coroutine as CR
+import Control.Coroutine.Stalling as SCR
 import Control.Monad.Eff
 import Control.Monad.Eff.Console
+import Control.Monad.Rec.Class as MR
+
+nats :: forall m. (Monad m) => SCR.StallingProducer Int m Unit
+nats = go 0
+  where
+    go i = do
+      SCR.emit i
+      go (i + 1)
+
+evens :: forall m. (MR.MonadRec m) => SCR.StallingProducer Int m Unit
+evens = SCR.filter (\x -> x `mod` 2 == 0) nats
+
+printer :: forall e. CR.Consumer String (Eff (console :: CONSOLE | e)) Unit
+printer =
+  CR.consumer \s ->
+    log s $> Nothing
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log "You should add some tests."
+  SCR.runStallingProcess (SCR.mapStallingProducer show evens SCR.$$? printer)
+  pure unit
